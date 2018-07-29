71f3a7046d2b0515b5995afa779591a8a9be05e9
JinLisek <tastyramen@gmail.com>
2018-07-29 17:50:54 +0200

    Added Keyboard Input handling in new thread and graphics output in another thread - STILL UGLY, needs FIXING

--- app/main.cpp
+++ app/main.cpp
@@ -1,18 +1,60 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 #include "TileRepresentation.hpp"
 #include "TileRepresentationFactory.hpp"
 #include "Map.hpp"
 #include "MapRepresentation.hpp"
 #include "TxtMapReader.hpp"
+#include "KeyboardInput.hpp"
 
-int main()
+bool QUIT = false;
+
+void getUserInput()
+{
+    char inputCharacter;
+    while(true)
+    {
+       inputCharacter = getch();
+
+        if(inputCharacter == 27)
+        {
+            QUIT = true;
+            break;
+        }
+    }
+}
+
+void clear() { //TODO: Change so it's not saved in console buffor
+    // CSI[2J clears screen, CSI[H moves the cursor to top-left corner
+    std::cout << "\x1B[2J\x1B[H";
+}
+
+void drawMap()
 {
     TxtMapReader mapReader{};
     auto map = mapReader.readMapFromFile("../Maps/DefaultMap.txt");
-
     MapRepresentation mapRepresentation {*map};
 
-    std::cout << mapRepresentation.draw() << std::endl;
+    while(true)
+    {
+        clear();
+        std::cout << mapRepresentation.draw() << std::endl;
+
+        if(QUIT)
+            break;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+}
+
+int main()
+{
+    std::thread keyboardInput(getUserInput);
+    std::thread graphicalOutput(drawMap);
+
+    keyboardInput.join();
+    graphicalOutput.join();
 
     return 0;
 }
