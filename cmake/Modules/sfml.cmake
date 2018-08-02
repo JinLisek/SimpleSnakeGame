53fea61865b156257ebba6f6cacf68c6eff690c7
JinLisek <tastyramen@gmail.com>
2018-08-02 16:22:45 +0200

    Changed sfml-audio to use C++ 98

--- cmake/Modules/sfml.cmake
+++ cmake/Modules/sfml.cmake
@@ -5,3 +5,5 @@ add_subdirectory(${SFML_DIR} ${CMAKE_BINARY_DIR}/sfml)
 include_directories(SYSTEM ${SFML_DIR}/sfml/include
                            ${SFML_DIR}/sfml/include)
 
+set_target_properties(sfml-audio PROPERTIES CXX_STANDARD 98 CXX_STANDARD_REQUIRED YES CMAKE_CXX_EXTENSIONS NO)
+
