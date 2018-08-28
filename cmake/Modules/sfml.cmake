6527cf68a3d420c45ca4691759999f1a5151a50b
JinLisek <tastyramen@gmail.com>
2018-08-28 19:50:17 +0200

    Fixed problems with GameLogic module in cmake and fixed a problem with OPEN_GL_PREFERENCE

--- cmake/Modules/sfml.cmake
+++ cmake/Modules/sfml.cmake
@@ -1,6 +1,8 @@
 set(SFML_DIR "deps/sfml/"
     CACHE PATH "The path to the SFML framework.")
 
+SET(OpenGL_GL_PREFERENCE "LEGACY")
+
 add_subdirectory(${SFML_DIR} ${CMAKE_BINARY_DIR}/sfml)
 include_directories(SYSTEM ${SFML_DIR}/sfml/include
                            ${SFML_DIR}/sfml/include)
